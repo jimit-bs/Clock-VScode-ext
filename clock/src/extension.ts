c88bbb2acc550fd009888954adb3cc4082b30a43
jimit-bs <jimit@browserstack.com>
2025-09-05 14:06:19 +0530

    unnecessary code deleted v3

--- clock/src/extension.ts
+++ clock/src/extension.ts
@@ -7,8 +7,6 @@ interface TimeResponse {
   currentTime: string;
 }
 let statusBarItem: vscode.StatusBarItem;
-let timerID: NodeJS.Timeout;
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
